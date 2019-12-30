f1b3d30cf96edd8f415fa767f2504ff2193d25c8
Emerson Costin <emerson.costin@dbm.com.br>
2019-12-30 13:27:29 -0300

    Photos

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -40,12 +40,13 @@ function addLocation() {
   const selected = select.options[select.selectedIndex];
   const geo = selected.value;
   const label = selected.textContent;
-  const location = {label: label, geo: geo};
+  const photo = selected.getAttribute("photo");
+  const location = {label: label, geo: geo, photo: photo};
   // Create a new card & get the weather data from the server
   const card = getForecastCard(location);
-  getForecastFromNetwork(geo).then((forecast) => {
-    renderForecast(card, forecast);
-  });
+  // getForecastFromNetwork(geo).then((forecast) => {
+  //   renderForecast(card, forecast);
+  // });
   // Save the updated list of selected cities.
   weatherApp.selectedLocations[geo] = location;
   saveLocationList(weatherApp.selectedLocations);
@@ -78,59 +79,15 @@ function renderForecast(card, data) {
   }
 
   // Find out when the element was last updated.
-  const cardLastUpdatedElem = card.querySelector('.card-last-updated');
-  const cardLastUpdated = cardLastUpdatedElem.textContent;
-  const lastUpdated = parseInt(cardLastUpdated);
 
-  // If the data on the element is newer, skip the update.
-  if (lastUpdated >= data.currently.time) {
-    return;
-  }
-  cardLastUpdatedElem.textContent = data.currently.time;
 
   // Render the forecast data into the card.
-  card.querySelector('.description').textContent = data.currently.summary;
-  const forecastFrom = luxon.DateTime
-      .fromSeconds(data.currently.time)
-      .setZone(data.timezone)
-      .toFormat('DDDD t');
-  card.querySelector('.date').textContent = forecastFrom;
-  card.querySelector('.current .icon')
-      .className = `icon ${data.currently.icon}`;
-  card.querySelector('.current .temperature .value')
-      .textContent = Math.round(data.currently.temperature);
-  card.querySelector('.current .humidity .value')
-      .textContent = Math.round(data.currently.humidity * 100);
-  card.querySelector('.current .wind .value')
-      .textContent = Math.round(data.currently.windSpeed);
-  card.querySelector('.current .wind .direction')
-      .textContent = Math.round(data.currently.windBearing);
-  const sunrise = luxon.DateTime
-      .fromSeconds(data.daily.data[0].sunriseTime)
-      .setZone(data.timezone)
-      .toFormat('t');
-  card.querySelector('.current .sunrise .value').textContent = sunrise;
-  const sunset = luxon.DateTime
-      .fromSeconds(data.daily.data[0].sunsetTime)
-      .setZone(data.timezone)
-      .toFormat('t');
-  card.querySelector('.current .sunset .value').textContent = sunset;
+ 
+  card.querySelector('.imagem').setAttribute("src", "http://www.dbm.com.br/wp-content/uploads/2018/03/quote-1.jpg")
 
   // Render the next 7 days.
-  const futureTiles = card.querySelectorAll('.future .oneday');
-  futureTiles.forEach((tile, index) => {
-    const forecast = data.daily.data[index + 1];
-    const forecastFor = luxon.DateTime
-        .fromSeconds(forecast.time)
-        .setZone(data.timezone)
-        .toFormat('ccc');
-    tile.querySelector('.date').textContent = forecastFor;
-    tile.querySelector('.icon').className = `icon ${forecast.icon}`;
-    tile.querySelector('.temp-high .value')
-        .textContent = Math.round(forecast.temperatureHigh);
-    tile.querySelector('.temp-low .value')
-        .textContent = Math.round(forecast.temperatureLow);
-  });
+  
+
 
   // If the loading spinner is still visible, remove it.
   const spinner = card.querySelector('.card-spinner');
@@ -181,6 +138,7 @@ function getForecastCard(location) {
   }
   const newCard = document.getElementById('weather-template').cloneNode(true);
   newCard.querySelector('.location').textContent = location.label;
+  newCard.querySelector('.imagem').setAttribute("src", location.photo);
   newCard.setAttribute('id', id);
   newCard.querySelector('.remove-city')
       .addEventListener('click', removeLocation);
@@ -200,10 +158,10 @@ function updateData() {
     // CODELAB: Add code to call getForecastFromCache
 
     // Get the forecast data from the network.
-    getForecastFromNetwork(location.geo)
-        .then((forecast) => {
-          renderForecast(card, forecast);
-        });
+    // getForecastFromNetwork(location.geo)
+    //     .then((forecast) => {
+    //       renderForecast(card, forecast);
+    //     });
   });
 }
 
@@ -234,7 +192,7 @@ function loadLocationList() {
   if (!locations || Object.keys(locations).length === 0) {
     const key = '40.7720232,-73.9732319';
     locations = {};
-    locations[key] = {label: 'New York City', geo: '40.7720232,-73.9732319'};
+    locations[key] = {label: 'Marechal', geo: '40.7720232,-73.9732319', photo: "http://www.dbm.com.br/wp-content/uploads/2018/03/slide2.jpg"};
   }
   return locations;
 }
--- server.js
+++ server.js
@@ -30,89 +30,7 @@ const API_KEY = process.env.DARKSKY_API_KEY;
 const BASE_URL = `https://api.darksky.net/forecast`;
 
 // Fake forecast data used if we can't reach the Dark Sky API
-const fakeForecast = {
-  fakeData: true,
-  latitude: 0,
-  longitude: 0,
-  timezone: 'America/New_York',
-  currently: {
-    time: 0,
-    summary: 'Clear',
-    icon: 'clear-day',
-    temperature: 43.4,
-    humidity: 0.62,
-    windSpeed: 3.74,
-    windBearing: 208,
-  },
-  daily: {
-    data: [
-      {
-        time: 0,
-        icon: 'partly-cloudy-night',
-        sunriseTime: 1553079633,
-        sunsetTime: 1553123320,
-        temperatureHigh: 52.91,
-        temperatureLow: 41.35,
-      },
-      {
-        time: 86400,
-        icon: 'rain',
-        sunriseTime: 1553165933,
-        sunsetTime: 1553209784,
-        temperatureHigh: 48.01,
-        temperatureLow: 44.17,
-      },
-      {
-        time: 172800,
-        icon: 'rain',
-        sunriseTime: 1553252232,
-        sunsetTime: 1553296247,
-        temperatureHigh: 50.31,
-        temperatureLow: 33.61,
-      },
-      {
-        time: 259200,
-        icon: 'partly-cloudy-night',
-        sunriseTime: 1553338532,
-        sunsetTime: 1553382710,
-        temperatureHigh: 46.44,
-        temperatureLow: 33.82,
-      },
-      {
-        time: 345600,
-        icon: 'partly-cloudy-night',
-        sunriseTime: 1553424831,
-        sunsetTime: 1553469172,
-        temperatureHigh: 60.5,
-        temperatureLow: 43.82,
-      },
-      {
-        time: 432000,
-        icon: 'rain',
-        sunriseTime: 1553511130,
-        sunsetTime: 1553555635,
-        temperatureHigh: 61.79,
-        temperatureLow: 32.8,
-      },
-      {
-        time: 518400,
-        icon: 'rain',
-        sunriseTime: 1553597430,
-        sunsetTime: 1553642098,
-        temperatureHigh: 48.28,
-        temperatureLow: 33.49,
-      },
-      {
-        time: 604800,
-        icon: 'snow',
-        sunriseTime: 1553683730,
-        sunsetTime: 1553728560,
-        temperatureHigh: 43.58,
-        temperatureLow: 33.68,
-      },
-    ],
-  },
-};
+
 
 /**
  * Generates a fake forecast in case the weather API is not available.
@@ -120,16 +38,7 @@ const fakeForecast = {
  * @param {String} location GPS location to use.
  * @return {Object} forecast object.
  */
-function generateFakeForecast(location) {
-  location = location || '-25.468039,-49.3001143';
-  const commaAt = location.indexOf(',');
 
-  // Create a new copy of the forecast
-  const result = Object.assign({}, fakeForecast);
-  result.latitude = parseFloat(location.substr(0, commaAt));
-  result.longitude = parseFloat(location.substr(commaAt + 1));
-  return result;
-}
 
 
 /**
@@ -138,23 +47,7 @@ function generateFakeForecast(location) {
  * @param {Request} req request object from Express.
  * @param {Response} resp response object from Express.
  */
-function getForecast(req, resp) {
-  const location = req.params.location || '-25.468039,-49.3001143';
-  const url = `${BASE_URL}/${API_KEY}/${location}`;
-  fetch(url).then((resp) => {
-    if (resp.status !== 200) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
-  }).then((data) => {
-    setTimeout(() => {
-      resp.json(data);
-    }, FORECAST_DELAY);
-  }).catch((err) => {
-    console.error('Dark Sky API Error:', err.message);
-    resp.json(generateFakeForecast(location));
-  });
-}
+
 
 /**
  * Starts the Express server.
@@ -179,9 +72,9 @@ function startServer() {
   });
 
   // Handle requests for the data
-  app.get('/forecast/:location', getForecast);
-  app.get('/forecast/', getForecast);
-  app.get('/forecast', getForecast);
+  //app.get('/forecast/:location', getForecast);
+ // app.get('/forecast/', getForecast);
+  //app.get('/forecast', getForecast);
 
   // Handle requests for static files
   app.use(express.static('public'));
